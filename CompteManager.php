319a4e4cfc344dbc6a75b61aab1ce46e2eb15a48
harounaKane <harouna.Kane@hotmail.fr>
2025-02-19 16:10:05 +0100

    creation de la classe manager pour utiliser la classe compte

---
+++ CompteManager.php
@@ -0,0 +1,24 @@
+<?php 
+
+class CompteManager{
+
+    public function ajouterCompte(){
+        echo "ajouter";
+    }
+
+    public function lireComptes(){
+        echo "lire tous";
+    }
+
+    public function lireUnCompte(){
+        echo "lire un";
+    }
+
+    public function supprimerComptes(){
+        echo "supprimer";
+    }
+
+    public function updateComptes(){
+        echo "update";
+    }
+}
